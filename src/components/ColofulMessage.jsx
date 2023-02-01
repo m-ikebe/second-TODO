e5fb23dccf8a51d39aef38baffd773e4899e52f6
m-ikebe <m-ikebe@pipjapan.co.jp>
2023-02-01 07:37:16 +0000

    Update 2 files

--- src/components/ColofulMessage.jsx
+++ src/components/ColofulMessage.jsx
@@ -7,7 +7,7 @@ const ColofulMessage = (props) => {
     // 分割代入を使用することで下記の通りオブジェクト名省略可能
     // color: props.color,
     color,
-    fontSize: "18px"
+    fontSize: "18px",
   };
   // ①変数に設定する方法
   // ②タグで囲う方法。
